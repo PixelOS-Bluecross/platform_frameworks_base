f34bef86680232394b50a7a1adf570db9ec87438
Danny Lin <danny@kdrag0n.dev>
2021-10-11 19:59:51 -0700

    Spoof build fingerprint for Google Play Services
    SafetyNet's CTS profile attestation checks whether Build.FINGERPRINT
    matches that of the device's stock OS, which has passed CTS testing.
    Spoof the fingerprint for Google Play Services to help pass SafetyNet.
    
    We used to set the real system build fingerprint to the stock one, but
    Android relies on each build having a unique fingerprint in order to
    clear the correct caches and update persistent state for system changes.
    On devices that no longer receive updates from the OEM, the build
    fingerprint never changes and Android doesn't account for updates
    correctly, which causes issues when updating without wiping data.
    Only spoofing the fingerprint for Google Play Services fixes this issue.
    
    Corresponding vendor commit:
    "Only use stock build fingerprint for Google Play Services"
    
    Change-Id: I26a2498eb2e2163933303b03f6d516e5fb30fe51
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -58,6 +58,8 @@ import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
 
+import com.android.internal.gmshooks.AttestationHooks;
+
 import java.io.File;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -1191,6 +1193,7 @@ public class Instrumentation {
                 .instantiateApplication(cl, className);
         app.attach(context);
         maybeSpoofBuild(app);
+        AttestationHooks.initApplicationBeforeOnCreate(app);
         return app;
     }
     
@@ -1209,6 +1212,7 @@ public class Instrumentation {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
         maybeSpoofBuild(app);
+        AttestationHooks.initApplicationBeforeOnCreate(app);
         return app;
     }
 
